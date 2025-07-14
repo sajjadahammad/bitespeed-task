031a3fdac85a6b5a2f18831e27b3f8cc5c307fb1
sajjadahammed <sajjad08@protonmail.com>
2025-07-14 23:42:37 +0530

    Add Radix UI components and Sonner for notifications; implement FlowBuilder and UI elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-
+import {ReactFlowProvider} from '@xyflow/react'
+import '@xyflow/react/dist/style.css';
+import FlowBuilder from './components/FlowBuilder';
 
 function App() {
 
   return (
     <>
-     
+     <ReactFlowProvider>
+      <FlowBuilder/>
+     </ReactFlowProvider>
     </>
   )
 }
---
+++ src/components/FlowBuilder.tsx
@@ -0,0 +1,6 @@
+
+export default function FlowBuilder() {
+  return (
+    <div>FlowBuilder</div>
+  )
+}
